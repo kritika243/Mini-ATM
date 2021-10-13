8a17e24ca03accf8818e446f1a2c239bf3e62777
kritika243 <24kritikasharma24@gmail.com>
2021-10-13 12:19:56 +0530

    changed logic

--- src/App.js
+++ src/App.js
@@ -8,27 +8,64 @@ function App() {
   const [amountDetails, setAmountDetails] = useState(null)
   const [balance, setBalance] = useState(0)
 
-  const notes = [2000, 500, 200, 100]
+  let deno = [2000, 500, 200, 100]
+  let n = deno.length
+  let noteCounter = Array(3).fill(0)
 
   const handleAmountChange = (e) => {
     setAmount(e.target.value)
   }
-  const handleSubmit = () => {
-    let notes_detail = []
-    function getNoteCount(a, n) {
-      let note = Math.floor(a / n)
-      amount = a % n
-      notes_detail.push([n, note])
-    }
-    notes.forEach((note) => {
-      if (amount >= note) {
-        getNoteCount(amount, note)
-        setBalance(amount)
-      } else {
-        setBalance(amount)
-      }
+  // const handleSubmit = () => {
+  //   let notes_detail = []
+  //   function getNoteCount(a, n) {
+  //     let note = Math.floor(a / n)
+  //     amount = a % n
+  //     notes_detail.push([n, note])
+  //   }
+  //   notes.forEach((note) => {
+  //     if (amount >= note) {
+  //       getNoteCount(amount, note)
+  //       setBalance(amount)
+  //     } else {
+  //       setBalance(amount)
+  //     }
+  //   })
+  //   setAmountDetails(notes_detail.reverse())
+  // }
+
+  // function handleSubmit() {
+  //   for (let i = 0; i < 9; i++) {
+  //     if (amount >= deno[i]) {
+  //       noteCounter[i] = Math.floor(amount / deno[i])
+  //       amount = amount - noteCounter[i] * deno[i]
+  //     }
+  //   }
+
+  //   // Printing notes
+  //   document.write('Currency Count ->' + '<br/>')
+  //   for (let i = 0; i < 9; i++) {
+  //     if (noteCounter[i] != 0) {
+  //       document.write(deno[i] + ' : ' + noteCounter[i] + '<br/>')
+  //     }
+  //   }
+  // }
+
+  function handleSubmit() {
+    var balance = {}
+    let remaining = [800, 300, 100, 0]
+    let idx = deno.findIndex((val) => {
+      return val < amount
     })
-    setAmountDetails(notes_detail.reverse())
+    let ans1, ans2
+    for (let i = idx; i < n; i++) {
+      ans1 = Math.floor(amount / deno[i])
+      ans2 = amount - ans1 * deno[i]
+      ans1 = ans2 >= remaining[i] ? ans1 : ans1 - 1
+
+      amount -= deno[i] * ans1
+      balance[deno[i]] = ans1
+    }
+    console.log(balance)
   }
 
   return (
@@ -42,7 +79,7 @@ function App() {
           />
         </div>
         <div>
-          <AtmTotal amountDetails={amountDetails} balance={balance} />
+          {/* <AtmTotal amountDetails={amountDetails} balance={balance} /> */}
         </div>
       </div>
     </div>
